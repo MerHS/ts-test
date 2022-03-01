6d94b124e041c8a58b1042c39c0c6ae671cb4364
MerHS <starvessel@naver.com>
2022-03-02 04:32:04 +0900

    basics

--- simple/fns.py
+++ simple/fns.py
@@ -1,89 +1,114 @@
 def add(a, b):
     return a.add(b)
 
+
 def add_(a, b):
     return a.add_(b)
+
 
 def sub(a, b):
     return a.sub(b)
 
+
 def sub_(a, b):
     return a.sub_(b)
+
 
 def mul(a, b):
     return a.mul(b)
 
+
 def mul_(a, b):
     return a.mul_(b)
+
 
 def div(a, b):
     return a.div(b)
 
+
 def div_(a, b):
     return a.div_(b)
+
+
+def add_distl(a, b, c):
+    return a.mul(b) + a.mul(c)
+
+
+def add_distr(a, b, c):
+    return b.mul(a) + c.mul(a)
+
+
+def add_dist_comm(a, b, c):
+    return a.mul(b) + c.mul(a)
+
 
 def mm(a, b):
     return a.mm(b)
 
-def mm_(a, b):
-    return a.mm_(b)
+
+def addmm(a, b, c):
+    return a.addmm(b, c)
+
+
+def mmmul(a, b, c):
+    return a.mm(b).mul(c)
+
+
+def mulmm(a, b, c):
+    return a.mul(b).mm(c)
+
 
 def addmul(a, b, c):
     return a.add(b).mul(c)
 
+
 def muladd(a, b, c):
     return a.mul(b).add(c)
+
 
 def muldiv(a, b, c):
     return a.mul(b).div(c)
 
+
 def divmul(a, b, c):
     return a.div(b).mul(c)
+
 
 def addmul_(a, b, c):
     return a.add_(b).mul_(c)
 
+
 def muladd_(a, b, c):
     return a.mul_(b).add_(c)
+
 
 def muldiv_(a, b, c):
     return a.mul_(b).div_(c)
 
+
 def divmul_(a, b, c):
     return a.div_(b).mul_(c)
+
 
 def muldivaba(a, b):
     return a.mul(b).div(a)
 
+
 def muldivabb(a, b):
     return a.mul(b).div(b)
+
 
 def divmulabb(a, b):
     return a.div(b).mul(b)
 
+
 def addsubabb(a, b):
     return a.add(b).sub(b)
+
 
 def addsubaba(a, b):
     return a.add(b).sub(a)
 
+
 def subaddabb(a, b):
     return a.sub(b).add(b)
-
-def muldivaba_(a, b):
-    return a.mul_(b).div_(a)
-
-def muldivabb_(a, b):
-    return a.mul_(b).div_(b)
-
-def divmulabb_(a, b):
-    return a.div_(b).mul_(b)
-
-def addsubabb_(a, b):
-    return a.add_(b).sub_(b)
-
-def addsubaba_(a, b):
-    return a.add_(b).sub_(a)
-
-def subaddabb_(a, b):
-    return a.sub_(b).add_(b)
--- simple_test.py
+++ simple_test.py
@@ -1,9 +1,10 @@
-
 import torch
 import argparse
 from tester import test_fn, test_fn_inplace
+from simple import fns, fns_script
+from inspect import signature
 
-ITER = 20000
+ITER = 50000
 CUDA_ITER = 50000
 
 torch.manual_seed(0)
@@ -29,7 +30,7 @@ if args.cuda:
     iter_n = CUDA_ITER
     cuda = True
     if not torch.cuda.is_available():
-        print('UNSUPPORTED: CUDA is not available')
+        print("UNSUPPORTED: CUDA is not available")
         exit(0x42)
 if args.fuse:
     torch._C._jit_set_texpr_fuser_enabled(True)
@@ -38,26 +39,34 @@ if args.nnc:
 if args.nvfuse:
     torch._C._jit_set_nvfuser_enabled(True)
 
-if args.ts_script:
-    from simple import fns_script as fns
-else:
-    from simple import fns
 
-
-device = 'cuda' if args.cuda else 'cpu'
+device = "cuda" if args.cuda else "cpu"
 size = [args.height, args.width]
 tensors = [torch.rand(size, device=device) for _ in range(16)]
 
+print(list(fns.__dict__.keys()))
+
 for fn_name in fns.__dict__.keys():
-    if fn_name.startswith('_') or fn_name == 'torch':
+    if fn_name.startswith("_") or fn_name == "torch":
         continue
 
     fn = fns.__dict__[fn_name]
+    fn_sig = signature(fn)
+    param_len = len(fn_sig.parameters)
+    params = tensors[:param_len]
+
+    if args.ts_script:
+        fn = fns_script.__dict__[fn_name]
 
     # if fn_name.endswith('_'):
     #     run_time = test_fn_inplace(fn, args, tensors, iter_n)
     # else:
     #     run_time = test_fn(fn, args, tensors, iter_n)
-    run_time = test_fn(fn, args, tensors, iter_n)
 
-    print(f'{run_time:8.5f}\t{fn_name}')
+    n = iter_n
+    if "mm" in fn_name:
+        n = iter_n // 20
+
+    run_time = test_fn(fn, args, params, n)
+
+    print(f"{run_time:8.5f}\t{fn_name}", flush=True)
